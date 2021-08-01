617554e259d5a0fc50075f0f53fc434adf15f8a8
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2021-08-01 19:39:04 +0600

    Refactor test helpers

--- tests/db.test.js
+++ tests/db.test.js
@@ -1,15 +1,14 @@
-import { MongoClient } from "mongodb";
 import { expect } from "chai";
 import { getUserByUsername } from "../src/db";
+import {
+    setDatabaseData,
+    getDatabaseData,
+    resetDatabase
+} from "./test-helpers";
+
 
 describe('getUserByUsername', () => {
     it('get the correct user from the database given a username', async () => {
-        const client = await MongoClient.connect('mongodb://localhost:27017/TEST_DB', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-
-        const db = client.db('TEST_DB');
         const fakeData = [
             {
                 id: 123,
@@ -23,12 +22,11 @@ describe('getUserByUsername', () => {
             }
         ];
 
-        await db.collection('users').insertMany(fakeData);
+        await setDatabaseData('users', fakeData);
 
         const actualResult = await getUserByUsername('abc');
-        const finalDBState = await db.collection('users').find().toArray();
-        await db.dropDatabase();
-        client.close();
+        const finalDBState = await getDatabaseData('users');
+        await resetDatabase();
 
         const expectedResult = {
             id: 123,
@@ -39,6 +37,6 @@ describe('getUserByUsername', () => {
         expect(actualResult).excludingEvery('_id').to.deep.equal(expectedResult);
         expect(finalDBState).excludingEvery('_id').to.deep.equal(fakeData);
 
-        
+
     });
 });
